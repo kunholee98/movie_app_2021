47edd896627b88a3c6109d5ef99aa94c83a4c25f
kunholee_98 <dlrjsgh750@naver.com>
2021-08-02 16:10:58 +0900

    food data로 연습

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
+
+import React from "react";
+
+function Food({name, rating}) {
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <h4>{rating}/5.0</h4>
+    </div>
+    );
+}
+
+const foodILike = [
+  {
+    id:1,
+    name: "kimchi",
+    rating: 5
+  },
+  {
+    id:2,
+    name: "potato",
+    rating: 4.5
+  },
+  {
+    id:3,
+    name: "sweetpotato",
+    rating: 4.7
+  }
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {foodILike.map(dish => (
+        <Food
+          key={dish.id}
+          name={dish.name}
+          rating={dish.rating}
+        />
+      ))}
     </div>
   );
 }
